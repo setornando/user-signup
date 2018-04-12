7ea2d8d3eac1fcedc0093fa8dd96cb199a532200
Jeff Michael <setornando@Jeffs-MacBook-Air.local>
2018-04-12 14:29:34 -0500

    Finished code

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, redirect, render_template
+import re
+
+
+app = Flask(__name__)
+app.config['DEBUG']= True
+
+app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/signup", methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+    password_confirmation = request.form['password_confirmation']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = '' 
+    confirmation_error = ''
+    email_error = ''
+
+    if username == "":
+        user_error = "Please enter a valid username."
+    elif len(username) < 3 or len(username) > 20:
+        username_error = "Username invalid. Please enter a username between 3 and 20 characters."
+        
+    elif " " in username:
+        username_error = "Username invalid. Username cannot contain spaces."
+        
+    
+    if password == "":
+        password_error = "Please enter a valid password."
+    elif len(password) < 3 or len(password) > 20:
+        password_error = "Password invalid. Please enter a password between 3 and 20 characters."
+        password = ""
+    elif " " in password:
+        password_error = "Password invalid. Password cannot contain spaces."
+        password = ""
+    
+    if password_confirmation == "" or password_confirmation != password:
+        confirmation_error = "Passwords do not match.Please re-enter."
+        password_confirmation = ""
+    
+    if email != "":
+
+        if len(email) < 3 or len(email) > 20:
+            email_error="The email address must be between 3 and 30 characters long."    
+        elif not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-z0-9-]+\.[a-zA-Z0-9-.]+$)",email):
+            email_error= "The email address must have a single @, and single ., and contain no spaces."
+    
+    if not username_error and not password_error and not confirmation_error and not email_error:
+        return render_template('welcome_page.html', username = username)
+    else:
+        return render_template('index.html', username = username, username_error = username_error, password_error = password_error, confirmation_error = confirmation_error,email = email, email_error = email_error)
+
+app.run()
